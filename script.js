c1bf8bf79296063a03a4461a494a59e0a711e443
byatriz <byatriz.gomes@escola.pr.gov.br>
2025-08-11 22:09:49 -0300

    01

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('meuBotao');
 
+    botao.addEventListener('click', () => {
+        alert('Você clicou no botão! O JavaScript está funcionando.');
+    });
+});
